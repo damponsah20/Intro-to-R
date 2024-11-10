0832a7b15aeddbc1e93be5d98592cd9a9e0ce0f7
AMPONSAH DANIEL <129934779+damponsah20@users.noreply.github.com>
2024-11-10 14:17:33 +0000

    Add files via upload

---
+++ FIRST ONE.R
@@ -0,0 +1,22 @@
+1+2
+log10(10)
+10 - 78
+x <- 5
+x
+y = 10
+y
+install.packages("dplyr", dependencies=TRUE)
+log10(10)
+x <- -7
+y = 9
+x*y
+install.packages("dplyr", dependencies=TRUE)
+install.packages("ggplot2", dependencies=TRUE)
+install.packages("rmarkdown", dependencies=TRUE)
+library(dplyr)
+library(ggplot2)
+vignette()
+a <- 10
+b = -10
+a**b
+a*b
